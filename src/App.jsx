34a733cbf340868bc55c9bb3eaf9471991eb74e5
Soul1 <darkwaud@gmail.com>
2020-02-19 14:38:25 +0300

    add state, added onClick in input

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,41 @@ import TodoListTasks from './Components/TodoListTasks'
 import TodoListFooter from './Components/TodoListFooter'
 
 class App extends React.Component {
-    tasks = [
-        {title:'CSS', isDone:true, priority: 'medium'},
-        {title:'JS', isDone:true, priority: 'high'},
-        {title:'HTML', isDone:true, priority: 'low'},
-        {title:'REACT', isDone:false, priority: 'high'},
-    ]
 
-    filterValue = 'All'
+    constructor(props) {
+        super(props);
+        this.newTaskTitleRef = React.createRef();
+    }
+
+    state = {
+        tasks: [
+            {title: 'CSS', isDone: true, priority: 'medium'},
+            {title: 'JS', isDone: true, priority: 'high'},
+            {title: 'HTML', isDone: true, priority: 'low'},
+            {title: 'REACT', isDone: false, priority: 'high'},
+        ],
+        filterValue: 'All',
+
+    }
+
+    onAddTaskClick = () => {
+        let text = this.newTaskTitleRef.current.value !== ''? this.newTaskTitleRef.current.value : 'Нет значения'
+        let newTask = {title: text, isDone: false, priority: 'low'};
+        let newTasks = [...this.state.tasks, newTask];
+        this.newTaskTitleRef.current.value = ''
+        this.setState({
+            tasks: newTasks,
+        });
+    }
 
     render = () => {
 
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader />
-                    <TodoListTasks tasks={this.tasks}/>
-                    <TodoListFooter filterValue={this.filterValue}/>
+                    <TodoListHeader onAddTaskClick={this.onAddTaskClick} refTitleTask={this.newTaskTitleRef}/>
+                    <TodoListTasks tasks={this.state.tasks}/>
+                    <TodoListFooter filterValue={this.state.filterValue}/>
                 </div>
             </div>
         );
